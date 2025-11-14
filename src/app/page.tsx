6a30cc68e6b442b3b71b6a30163e97cb17bfd31a
IanMose <ngocheian@gmail.com>
2025-11-14 16:06:34 +0300

    Updating with master

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // src/app/page.tsx
-import SignInPage from "@/app/auth/signin/page";
-import AuthLayout from "@/app/auth/layout/authlayout";
+import SignInPage from "@/app/(auth)/signin/page";
+import AuthLayout from "@/app/(auth)/layout/authlayout";
 export default function HomePage() {
   return (
     <AuthLayout imageSrc="/login.jpg">
